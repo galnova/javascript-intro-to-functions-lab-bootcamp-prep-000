94dddbb9016b91b2a34a5ddf558266fb76fbe74a
Keith Jeter <kjeter2001@yahoo.com>
2017-12-18 18:44:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma() {
-  if
+    if (time < 10) {
+      greeting = "Good morning";
+  } else if (time < 20) {
+      greeting = "Good day";
+  } else {
+      greeting = "Good evening";
+  }
 }
